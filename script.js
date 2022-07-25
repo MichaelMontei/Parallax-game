70625dd07352cc4be691e248352343f4ca4972ac
MichaelMontei <code-me@hotmail.com>
2022-07-25 10:02:09 +0200

    script.js is born, assets are born, index.html is born and style.css is born

---
+++ script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('canvas1');
+
+// Create an instance of build-in 2d
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+
+// Scroll speed to slow down or speed up dynamically
+let gameSpeed = 5;
+
+const layer1 = new Image();
+layer1.src = 'layer1.png';
+const layer2 = new Image();
+layer1.src = 'layer1.png';
+const layer3 = new Image();
+layer1.src = 'layer1.png';
+const layer4 = new Image();
+layer1.src = 'layer1.png';
+const layer5 = new Image();
+layer1.src = 'layer1.png';
+const layer1 = new Image();
+layer1.src = 'layer1.png';
+const layer1 = new Image();
+layer1.src = 'layer1.png';
+const layer1 = new Image();
+layer1.src = 'layer1.png';
+const layer1 = new Image();
+layer1.src = 'layer1.png';
