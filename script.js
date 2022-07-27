f5922c6e8a5c32bdf5f26cba547598e7c84f1fe0
MichaelMontei <code-me@hotmail.com>
2022-07-27 16:28:51 +0200

    updated the canvas vision, made player movement and incoming object to cause collisions, collisions not yet working

--- script.js
+++ script.js
@@ -1,62 +1,51 @@
-// window.addEventListener('load', function() {
-    const canvas = document.getElementById('canvas1');
+const canvas = document.getElementById('canvas1');
 
-// Create an instance of build-in 2d
-    const ctx = canvas.getContext('2d');
-    const CANVAS_WIDTH = canvas.width = 1920;
-    const CANVAS_HEIGHT = canvas.height = 1080;
-// });
-// Scroll speed to slow down or speed up dynamically
-let gameSpeed = 5;
+//Create an instance of build-in 2d
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 1920;
+const CANVAS_HEIGHT = canvas.height = 1080;
+
+//Scroll speed to slow down or speed up dynamically
+let gameSpeed = 4;
+let planeInterval = 1000;
 const keys = [];
+const planesArray = [];
 
+//Assets to add all images
 const background = new Image();
-background.src = 'layer1.png';
+background.src = "assets/layer1.png";
 const cloud1 = new Image();
-cloud1.src = 'layer2.png';
+cloud1.src = "assets/layer2.png";
 const cloud2 = new Image();
-cloud2.src = 'layer3.png';
+cloud2.src = "assets/layer3.png";
 const cloud3 = new Image();
-cloud3.src = 'layer4.png';
+cloud3.src = "assets/layer4.png";
 const cloud4 = new Image();
-cloud4.src = 'layer5.png';
+cloud4.src = "assets/layer5.png";
 const cloud5 = new Image();
-cloud5.src = 'layer6.png';
+cloud5.src = "assets/layer6.png";
 const cloud6 = new Image();
-cloud6.src = 'layer7.png';
+cloud6.src = "assets/layer7.png";
 const bushes = new Image();
-bushes.src = 'layer8.png';
+bushes.src = "assets/layer8.png";
 const distant = new Image();
-distant.src = 'layer9.png';
+distant.src = "assets/layer9.png";
 const trees = new Image();
-trees.src = 'layer10.png';
+trees.src = "assets/layer10.png";
 const ground = new Image();
-ground.src = 'layer11.png';
-const ship = new Image();
-ship.src = 'layer12.png';
+ground.src = "assets/layer11.png";
+
+//PLanes for collisions with the player
 const ship2 = new Image();
-ship2.src = 'layer13.png';
+ship2.src = "assets/layer13.png";
 const ship3 = new Image();
-ship3.src = 'layer14.png';
+ship3.src = "assets/layer14.png";
 
+//Images for the player
 const playerImage = new Image();
-playerImage.src = 'jawa.png';
-console.log(playerImage);
+playerImage.src = "assets/jawa.png";
 
-// const player = {
-//     x: 0,
-//     y: 0,
-//     width: 32,
-//     height: 48,
-//     frameX: 0,
-//     frameY: 0,
-//     speed: 9,
-//     moving: false
-// };
-
-// const playerSprite = new Image();
-// playerSprite.src = "jawa.png";
-
+//Layer class to add all layers
 class Layer {
     constructor(image, speedModifier){
         this.x = 0;
@@ -85,25 +74,46 @@ class Layer {
     }
 }
 
+//Player class
 class Player {
 
-    constructor(image, speed) {
+    constructor(image, speed, width, height) {
         this.image = image;
-        this.x = 300;
-        this.y = 800;
-        this.width = 300;
-        this.height = 300;
+        this.x = 100;
+        this.y = 650;
+        this.width = width;
+        this.height = height;
         this.speed = speed;
     }
     draw() {
         ctx.drawImage(playerImage, this.x, this.y, player.width, player.height);
+
     }
 }
 
+class Plane {
+    constructor(image, width, height, speed){
+        this.image = image;
+        this.x = 1900;
+        this.y = Math.random() * canvas.height;
+        this.width = 1500;
+        this.height = 1500;
+        this.speed = speed;
+    }
+    update() {
+        this.x -= this.speed;
+    }
+    draw () {
+        ctx.drawImage(ship3 ,this.x, this.y, this.width, this.height)
+    }
+}
+
+function drawPlane (img, ship3X, ship3Y, ship3Width, ship3Height) {
+    ctx.drawImage(img, ship3X, ship3Y, ship3Width, ship3Height)
+}
+
+//Create all the layers
 const layer1 = new Layer(background, 0);
-const layer13 = new Layer(ship2, 1.7);
-const layer14 = new Layer(ship3, 2);
-const layer12 = new Layer(ship, 1.4);
 const layer2 = new Layer(cloud1, 0.9);
 const layer3 = new Layer(cloud2, 0.8);
 const layer4 = new Layer(cloud3, 0.7);
@@ -114,7 +124,18 @@ const layer8 = new Layer(bushes, 0.5);
 const layer9 = new Layer(distant, 0.6);
 const layer10 = new Layer(trees, 1);
 const layer11 = new Layer(ground, 1);
-const player = new Player(playerImage, 5);
+
+
+//Make array for all the layers
+const layerArray = [layer1, layer2, layer3, layer4, layer5, layer6, layer7, layer8, layer9, layer10, layer11];
+
+
+//Create the player
+const player = new Player(playerImage, 5, 350, 350);
+const spaceShip = new Player(ship3, 5);
+let test = spaceShip;
+const plane = new Plane(test, 500, 500, 6);
+
 
 window.addEventListener('keydown', (e) => {
     keys[e.key] = true;
@@ -124,51 +145,72 @@ window.addEventListener('keyup', (e) => {
     delete keys[e.key];
 })
 
-function animate() {
+function animate(timestamp) {
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
     // drawSprite(playerSprite, 0, 0, player.width, player.height, 0, 0, player.height, player.width);
-    layer1.update();
-    layer1.draw();
-    layer2.update();
-    layer2.draw();
-    layer3.update();
-    layer3.draw();
-    layer4.update();
-    layer4.draw();
-    layer5.update();
-    layer5.draw();
-    layer6.update();
-    layer6.draw();
-    layer7.update();
-    layer7.draw();
-    layer8.update();
-    layer8.draw();
-    layer9.update();
-    layer9.draw();
-    layer10.update();
-    layer10.draw();
-    layer11.update();
-    layer11.draw();
-    layer12.update();
-    layer12.draw();
-    layer13.update();
-    layer13.draw();
-    layer14.update();
-    layer14.draw();
 
-    if(keys['ArrowUp']){
+    //Instead of updating and drawing each layer separately, we will use the layerArray here
+    layerArray.forEach(layer =>{
+        layer.update();
+        layer.draw();
+    })
+    drawPlane(test.image, spaceShip.x, spaceShip.y, spaceShip.width, spaceShip.height);
+
+    if(keys['ArrowUp'] && player.y > -215){
         player.y -= player.speed;
     }
-    if(keys['ArrowRight']){
-        player.x += player.speed;
-    }
-    if(keys['ArrowLeft']){
-        player.x -= player.speed;
-    }
-    if(keys['ArrowDown']){
+    // if(keys['ArrowRight']){
+    //     player.x += player.speed;
+    // }
+    // if(keys['ArrowLeft']){
+    //     player.x -= player.speed;
+    // }
+    if(keys['ArrowDown'] && player.y < 780){
         player.y += player.speed;
     }
     player.draw();
-    requestAnimationFrame(animate)
+    plane.draw();
+    animateObstacles();
+    requestAnimationFrame(animate);
 }
 animate();
+setTimeout(() => {
+    generatePlanes();
+    console.log(planesArray)
+}, 0);
+
+
+function generatePlanes () {
+    if (Math.round(Math.random())) {
+        planesArray.push(new Plane(ship2,80, 80,6))
+        planesArray.push(new Plane(ship2,80, 80,6))
+    }
+    else{
+        planesArray.push(new Plane("assets/layer14.png" ,80, 80, 8));
+        planesArray.push(new Plane("assets/layer13.png" ,80, 80, 12));
+    }
+    setTimeout(generatePlanes, planeInterval);
+}
+
+function animateObstacles () {
+    planesArray.forEach((planes, index) => {
+        planes.draw();
+        planes.update();
+        //removes items from array when they leave screen
+        if ((planesArray.x + planesArray.width) <=0) {
+            setTimeout(() => {
+                planesArray.splice(index, 1);
+            }, 0)
+        }
+
+        //detects collision between player and obstacles
+        if (test.x < planes.x + planes.width &&
+            test.x + test.width > planes.x &&
+            test.y < planes.y + planes.height &&
+            test.y + test.height > planes.y
+        ) {
+            alert('ya dead buddy');
+            // location.reload();
+        }
+    });
+}
